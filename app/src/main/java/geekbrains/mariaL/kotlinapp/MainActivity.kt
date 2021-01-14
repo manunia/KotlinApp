6b36ae6205a924dd5ea970af1b5a272bc0e8dab1
Mariya <mariya.lupandina@simbirsoft.com>
2021-01-14 14:56:31 +0300

    добавлена кнопка + проверка работы

--- app/src/main/java/geekbrains/mariaL/kotlinapp/MainActivity.kt
+++ app/src/main/java/geekbrains/mariaL/kotlinapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package geekbrains.mariaL.kotlinapp
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.button)
+
+        button.setOnClickListener{
+            (it as Button).text = "Pressed"
+        }
+
     }
 }
