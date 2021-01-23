dc06551275c349e2b7bd384f68fbbbe7f4853f04
Mariya <mariya.lupandina@simbirsoft.com>
2021-01-23 21:46:41 +0300

    исправлены класс и активити заметок

--- app/src/main/java/geekbrains/mariaL/kotlinapp/model/Note.kt
+++ app/src/main/java/geekbrains/mariaL/kotlinapp/model/Note.kt
@@ -1,5 +1,44 @@
 package geekbrains.mariaL.kotlinapp.model
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.*
 
-class Note (val title: String, val severity: String, val date: String, val summary: String, val note: String, val color: Int) : Serializable
+@Parcelize
+data class Note (
+        val id: String,
+        val title: String,
+        val severity: String,
+        val modifyDate: Date = Date(),
+        val summary: String,
+        val note: String,
+        val color: Int) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Note
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (severity != other.severity) return false
+        if (modifyDate != other.modifyDate) return false
+        if (summary != other.summary) return false
+        if (note != other.note) return false
+        if (color != other.color) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + severity.hashCode()
+        result = 31 * result + modifyDate.hashCode()
+        result = 31 * result + summary.hashCode()
+        result = 31 * result + note.hashCode()
+        result = 31 * result + color
+        return result
+    }
+}
