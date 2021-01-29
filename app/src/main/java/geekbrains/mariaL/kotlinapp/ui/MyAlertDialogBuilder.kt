e4902ec42e025918f968ce5a118f25fd9faa1284
Mariya <mariya.lupandina@simbirsoft.com>
2021-01-29 14:08:50 +0300

    добавлены базовые классы и алерт диалог с сообщением

---
+++ app/src/main/java/geekbrains/mariaL/kotlinapp/ui/MyAlertDialogBuilder.kt
@@ -0,0 +1,19 @@
+package geekbrains.mariaL.kotlinapp.ui
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+
+class MyAlertDialogBuilder(val context : Context, val title: String, val message : String = "") {
+
+
+    fun build() {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("ОК") {
+                    dialog, id ->  dialog.cancel()
+                }
+        builder.create()
+    }
+
+}
