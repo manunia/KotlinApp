38da3c3678fc9e4d2873783f5dc4d87cb6f62e87
Mariya <mariya.lupandina@simbirsoft.com>
2021-02-20 08:35:26 +0300

    рефакторинг слоя данных

--- app/src/main/java/geekbrains/mariaL/kotlinapp/repo/Repository.kt
+++ app/src/main/java/geekbrains/mariaL/kotlinapp/repo/Repository.kt
@@ -6,10 +6,10 @@ import geekbrains.mariaL.kotlinapp.providers.RemoteDataProvider
 
 class Repository(private val remoteProvider: RemoteDataProvider) {
 
-    fun getNotes() = remoteProvider.subscribeToAllNotes()
-    fun saveNote(note: Note) = remoteProvider.saveNote(note)
-    fun getNoteById(id: String) = remoteProvider.getNoteById(id)
-    fun getCurrentUser() = remoteProvider.getCurrentUser()
-    fun deleteNote(noteId: String) = remoteProvider.deleteNote(noteId)
+    suspend fun getNotes() = remoteProvider.subscribeToAllNotes()
+    suspend fun saveNote(note: Note) = remoteProvider.saveNote(note)
+    suspend fun getNoteById(id: String) = remoteProvider.getNoteById(id)
+    suspend fun getCurrentUser() = remoteProvider.getCurrentUser()
+    suspend fun deleteNote(noteId: String) = remoteProvider.deleteNote(noteId)
 
 }
